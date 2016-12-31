1c5ad91e2b2bc4f5c7a6f22d21514d1bd3e47195
ymnkJT <javadtomi@gmail.com>
2016-12-31 14:43:41 +0900

    Add column of image, content and create database, pictures table

--- db/migrate/20161231051457_create_pictures.rb
+++ db/migrate/20161231051457_create_pictures.rb
@@ -1,7 +1,8 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-
+      t.text       :image
+      t.text       :content
       t.timestamps null: false
     end
   end
