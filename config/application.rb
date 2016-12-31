5141aaa024391403157666e2a49b62593c0ebc46
ymnkJT <javadtomi@gmail.com>
2016-12-31 23:29:16 +0900

    Set langage from en to ja

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ Bundler.require(*Rails.groups)
 
 module Instagram
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
