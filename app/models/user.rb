60e7b4309af54c4cbcdf054d6ff6f09ffce5a5e7
ymnkJT <javadtomi@gmail.com>
2017-01-01 22:27:14 +0900

    Add a function of sending authentication mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :pictures
 end
