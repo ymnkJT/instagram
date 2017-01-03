468d6f25c2b4c21b8ac8280d93731ce587ed0f10
ymnkJT <javadtomi@gmail.com>
2017-01-03 15:52:21 +0900

    First set the authentication function for sns

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pictures#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
