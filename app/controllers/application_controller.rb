e6b17d3ab16a25feff867d2f51f45a5d1193f4bf
ymnkJT <javadtomi@gmail.com>
2017-01-03 18:07:52 +0900

    Add the function of upload a avatar image and edit index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  PERMISSIBLE_ATTRIBUTES = %i(name)
+  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
   protected
     def configure_permitted_parameters
